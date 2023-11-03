c38adcc25e2e38f0af5af05473b404ad4d949f08
NemiMoser <135074383+NemiMoser@users.noreply.github.com>
2023-11-02 21:09:41 -0400

    edited connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,9 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {logging: console.log,
+  });
+  sequelize.options.logging = console.log;
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -13,9 +15,22 @@ if (process.env.JAWSDB_URL) {
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3301
+      port: 3306
     }
   );
 }
+
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;
